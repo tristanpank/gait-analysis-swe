0949b789c70f411ab67559e77469740715a8a127
tristanpank <tristanpank@gmail.com>
2024-03-01 12:48:50 -0500

    Added test for pose

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -7,3 +7,8 @@ def test_hello_world():
   response = client.get("/")
   assert response.status_code == 200
   assert response.json() == {"message": "Hello World"}
+
+def test_detect_pose():
+  # Error if no video passed
+  res = client.post("/pose/")
+  assert res.status_code == 422
