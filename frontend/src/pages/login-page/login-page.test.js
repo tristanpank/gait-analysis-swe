27abb7491d46ee1414b512990b22943bb7ba1dd5
DiegoCarra <forgamesonly057@gmail.com>
2024-03-02 12:35:38 -0500

    Add login test

---
+++ frontend/src/pages/login-page/login-page.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import Login from './login-page.js';
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+test('renders login page', () => {
+    render(
+    <Router>
+        <Routes>
+        <Route path="/" element={<Login/>} />
+        </Routes>
+    </Router>
+    );
+
+    const helloElement = screen.getByTestId('Login Form');
+    expect(helloElement).toBeInTheDocument();
+    
+    
+});
