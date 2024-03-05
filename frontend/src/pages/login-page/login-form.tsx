711b2410f089adc8361738645bedab7d7eca9480
csebastianpaez <csebastianpaez@outlook.com>
2024-03-05 03:03:48 -0500

    Update title and button

--- frontend/src/pages/login-page/login-form.tsx
+++ frontend/src/pages/login-page/login-form.tsx
@@ -55,7 +55,7 @@ export function LoginForm(props) {
     return (
         <div data-testid="Login Form" className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
             <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-            Login
+            Log In
             </h2>
             <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
             Don't have an account? <a className="text-blue-500 hover:text-blue-700 cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" onClick={handleClick} >Sign Up</a>
@@ -76,7 +76,7 @@ export function LoginForm(props) {
                 className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
                 type="submit"
             >
-                Login &rarr;
+                Log In &rarr;
                 <BottomGradient />
             </button>
 
