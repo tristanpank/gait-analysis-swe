3ffc7de00ce904fbaf7e77526343a3d5203c2ce2
DiegoCarra <forgamesonly057@gmail.com>
2024-03-08 18:42:08 -0500

    Add hero parallax

--- frontend/src/pages/home-page/home-page.js
+++ frontend/src/pages/home-page/home-page.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import LoginButton from './login-button.js';
 import Header from "../../components/ui/header/header.js";
+import { HomeParallax } from './home-parallax.js';
 
 const Home = () => {
 
   return (
     <div>
       <Header />
-      <div className="text-black text-7xl mt-44 text-center">
-        UF Gait Analysis
-      </div>
-      <div className="text-black text-2xl mt-5 text-center">
-        A platform that analyzes running form for student athletes at UF, making the resources of expensive labs available for everyday runners
-      </div>
-      <div className="flex justify-center items-center mt-8">
-        <LoginButton />
-      </div>
+      <HomeParallax />
     </div>
   );
 }
