886ecf1e8d5c73a89343af08834a7bec38b88f96
DiegoCarra <forgamesonly057@gmail.com>
2024-03-09 15:52:21 -0500

    Make pfp the profile button

--- frontend/src/components/ui/header/header.js
+++ frontend/src/components/ui/header/header.js
@@ -97,14 +97,11 @@ const Header = (props) => {
         <header className={"flex z-[10] fixed top-0 w-screen p-0 m-0 border-box " + headerColor}>
             <div className="flex justify-between items-center w-full ">
                 <h1 className="px-2 py-4 font-semibold text-2xl">Gait Analysis</h1>
-                <ProfileIcon user={user} setUser={setUser} />
             </div>
             <div className="flex mr-5">
                 <Sheet>
                     <SheetTrigger>
-                        <Button size="icon">
-                            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                        </Button>
+                        <ProfileIcon user={user} setUser={setUser} />
                     </SheetTrigger>
                     <SheetContent>
                         <SheetHeader>
