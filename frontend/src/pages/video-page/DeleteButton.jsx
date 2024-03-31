0ef56fc317921a57b64f9002ba378daf5447beb3
csebastianpaez <csebastianpaez@outlook.com>
2024-03-31 19:18:07 -0400

    Add loading icon for delete

--- frontend/src/pages/video-page/DeleteButton.jsx
+++ frontend/src/pages/video-page/DeleteButton.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger, DialogTitle, DialogHeader, DialogClose } from "../../shadcn/components/ui/dialog";
 import { Button } from "../../shadcn/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { deleteVideo } from "../../firebase/db";
 import { GlobalStateContext } from "src/components/react/GlobalStateProvider";
+import { Loader2 } from "lucide-react";
 
 
 export default function DeleteButton(props) {
   const { user, vid } = props;
+  const [ loading, setLoading ] = useState(false)
   const navigate = useNavigate();
   const { setVideoUploaded } = React.useContext(GlobalStateContext);
 
   async function handleDelete() {
+    setLoading(true)
     const result = await deleteVideo(user, vid);
     if (result === true) {
       setVideoUploaded(true);
@@ -29,10 +33,24 @@ export default function DeleteButton(props) {
           <DialogTitle>Are you sure you want to delete?</DialogTitle>
         </DialogHeader>
         <div className="flex justify-end space-x-2">
-          <DialogClose>
+          {!loading && (
+            <div>
+              <DialogClose>
+                <Button variant="outline" >Cancel</Button>
+              </DialogClose>
+              <Button onClick={handleDelete}>Delete</Button>
+            </div>
+          )}
+          {loading && (
+            <Button disabled>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          )}
+          {/* <DialogClose>
             <Button variant="outline" >Cancel</Button>
           </DialogClose>
-          <Button onClick={handleDelete}>Delete</Button>
+          <Button onClick={handleDelete}>Delete</Button> */}
         </div>
       </DialogContent>
     </Dialog>
