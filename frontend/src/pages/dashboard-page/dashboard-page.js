75477090f9d6e3ad3411445c35895f2de45a4ebe
DiegoCarra <forgamesonly057@gmail.com>
2024-04-20 12:29:20 -0400

    Refactor dashboard-page.js to improve video card styling

--- frontend/src/pages/dashboard-page/dashboard-page.js
+++ frontend/src/pages/dashboard-page/dashboard-page.js
@@ -28,18 +28,20 @@ const Dashboard = (props) => {
   }, [user, videoUploaded])
   
   const content = Array.isArray(videoArray) ? videoArray.map((vid, index) => (
-    <div key={index}><VideoCard user={user} vid={vid}></VideoCard></div>
+    <div key={index} className="bg-white rounded-md my-2">
+      <VideoCard user={user} vid={vid}></VideoCard>
+    </div>
   )) : null;
 
   return (
-    <div className=" bg-gradient-to-r from-[#5392b5] to-[#2b5981e3] min-h-[100dvh]">
+    <div className="bg-slate-100 min-h-screen">
       <Header user={user} setUser={setUser} color={true}></Header>
       <div className="mt-15 pt-20">
-        <button className="w-full h-20" onClick={handleClick}>
+        {/* <button className="w-full h-20" onClick={handleClick}>
           <div className='mx-5 py-7 h-full border'>Upload a video</div>
-        </button>
-        <div className='flex justify-center h-screen'>
-          <div className='bg-white md:max-w-[40%] mt-5 p-5 divide-y-[1px]'>
+        </button> */}
+        <div className='flex justify-center md:w-[40%] m-auto'>
+          <div className='divide-y-[1px]'>
               {content}
           </div>
           {/* <div className="hidden md:block w-0 bg-white md:my-5 md:p-5 md:w-full text-black">Heyyy</div> */}
